158cc0e53bc3707b48b8f1c8b83984045e02a15b
Shaun Bharat <79479981+shaunbharat@users.noreply.github.com>
2026-03-26 19:40:45 -0400

    one

--- challenges/easy/lambda_function1.py
+++ challenges/easy/lambda_function1.py
@@ -16,7 +16,7 @@ def lambda_handler(event, context=None):
             'body': json.dumps('Error: temperature field is required')
         }
     
-    fahrenheit = celsius * 9/5 + 38
+    fahrenheit = celsius * 9/5 + 32
     if type(fahrenheit) == float:
         fahrenheit = round(fahrenheit, 2) 
     
